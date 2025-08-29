be8301448988af00d903d28a8c6a0da53121cb6c
Nathan Viana <oliveiranathan700@gmail.com>
2025-08-28 23:04:01 -0300

    feat: criar layout de login e cadastro

--- app/src/main/java/com/aprendizado/teste_me/Login.kt
+++ app/src/main/java/com/aprendizado/teste_me/Login.kt
@@ -8,21 +8,22 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Cadastro : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_cadastro)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+
         val textField = findViewById<TextView>(R.id.cliqueAqui)
 
         textField.setOnClickListener {
-            val intent = Intent(this, Login::class.java)
+            val intent = Intent(this, Cadastro::class.java)
             startActivity(intent)
             finish()
         }
